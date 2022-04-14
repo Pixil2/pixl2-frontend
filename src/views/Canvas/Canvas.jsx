1536cd0796ed8ead651bc82667c4c293e70c8610
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-14 10:57:40 -0700

    connecting grid component to canvas view

--- src/views/Canvas/Canvas.jsx
+++ src/views/Canvas/Canvas.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Grid from '../../components/Canvas/Grid';
 
 export default function Canvas() {
-  return <div>Canvas</div>;
+  return (
+    <div>
+      <Grid />
+    </div>
+  );
 }
