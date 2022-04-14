8098672e4f1ca2eb58476eb2b99d051088fc4ead
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-14 11:28:25 -0700

    got canvas grid rendered

--- src/components/Canvas/Grid.jsx
+++ src/components/Canvas/Grid.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import styles from './Grid.css';
+import Pixl from './Pixl';
 
 export default function Grid() {
-  return <div>Grid</div>;
+  const array = [];
+  for (let i = 0; i < 100; i++) {
+    array.push(<Pixl />);
+  }
+  return <div className={styles.Grid}>{array.map((item) => item)}</div>;
 }
---
+++ src/components/Canvas/Pixl.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Pixl.css';
+
+export default function Pixl() {
+  return <div className={styles.Pixl}></div>;
+}
