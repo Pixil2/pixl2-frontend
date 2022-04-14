82878de450c9d176c63aa06d11d64a0158fc432b
ajoy267 <andrew.joy12@hotmail.com>
2022-04-14 15:28:47 -0700

    cleaning up code

--- src/components/Canvas/Grid.jsx
+++ src/components/Canvas/Grid.jsx
@@ -8,20 +8,17 @@ export default function Grid() {
   const { image } = useContext(imageContext);
   const array = renderImage(image);
 
-  const area = image.height * image.width;
-  useEffect(() => {
-    const res = createImage('title', 10, 10);
-  }, []);
-
   return (
-    <div
-      className={styles.Grid}
-      style={{
-        gridTemplateColumns: `repeat(${image.width}, 1fr)`,
-        gridTemplateRows: `repeat(${image.height}, 1fr)`,
-      }}
-    >
-      {array.map((item) => item)}
+    <div>
+      <div
+        className={styles.Grid}
+        style={{
+          gridTemplateColumns: `repeat(${image.width}, 1fr)`,
+          gridTemplateRows: `repeat(${image.height}, 1fr)`,
+        }}
+      >
+        {array.map((item) => item)}
+      </div>
     </div>
   );
 }
