8ae5f196bf7b3a0bed4f33de04aa518548a3961a
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-15 10:18:23 -0700

    setting footer into position

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from './Footer.css';
 export default function Footer() {
-  return <div>Footer</div>;
+  return <div className={styles.Footer}>Things That A Footer Might Have</div>;
 }
