9ce69711ab7ad52a93138cb9f36b5cf2f28d7660
ajoy267 <andrew.joy12@hotmail.com>
2022-04-15 09:49:59 -0700

    styled the ehader a little bit

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.css';
 
 export default function Header() {
   return (
-    <header>
-      <h1>PIXL</h1>
+    <header className={styles.Header}>
+      <h1 className={styles.logo}>PIXL</h1>
       <Link to="/profile">
-        <p>Gallery</p>
+        <p className={styles.gallery}>Gallery</p>
       </Link>
     </header>
   );
