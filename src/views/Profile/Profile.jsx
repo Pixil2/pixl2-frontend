2fa14a45e1aacdb921d92a93e26dea3d7cbad1db
ajoy267 <andrew.joy12@hotmail.com>
2022-04-18 10:13:01 -0700

    got the popup button to start working

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -4,9 +4,11 @@ import Grid from '../../components/Canvas/Grid';
 import { getCurrentUser } from '../../services/users';
 import { v4 as uuid } from 'uuid';
 import styles from './Profile.css';
+import PopUp from '../PopUp/PopUp';
 
 export default function Profile() {
   const [currentImages, setCurrentImages] = useState([]);
+  const [popUp, setPopUp] = useState(false);
 
   useEffect(() => {
     const fetch = async () => {
@@ -18,11 +20,23 @@ export default function Profile() {
     fetch();
   }, []);
 
+  const handlePopup = () => {
+    setPopUp(!popUp);
+  };
+
   return (
-    <div className={styles.Profile}>
-      {currentImages.map((item) => {
-        return <Grid key={uuid()} image={item} />;
-      })}
+    <div>
+      <button onClick={handlePopup}>Create Image</button>
+      {popUp && <PopUp />}
+      <div className={styles.Profile}>
+        {currentImages.map((item) => {
+          return (
+            <div>
+              <Grid key={uuid()} image={item} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
