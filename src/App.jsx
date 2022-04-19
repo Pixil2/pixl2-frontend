8283916470f93644e814e79909d23b562e6da279
ajoy267 <andrew.joy12@hotmail.com>
2022-04-19 15:37:19 -0700

    attempting protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AboutUs from './views/AboutUs/AboutUs';
 import Canvas from './views/Canvas/Canvas';
 import Home from './views/Home/Home';
 import Profile from './views/Profile/Profile';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import styles from './App.css';
 
 export default function App() {
