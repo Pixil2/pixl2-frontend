e1e2c028d420a9de92ae787ae59ee6121519def9
ajoy267 <andrew.joy12@hotmail.com>
2022-04-19 16:20:19 -0700

    got the protected routes working and added on app.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Canvas from './views/Canvas/Canvas';
 import Home from './views/Home/Home';
 import Profile from './views/Profile/Profile';
 import styles from './App.css';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 export default function App() {
   return (
@@ -11,9 +12,25 @@ export default function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/profile" element={<Profile />} />
+          <Route
+            exact
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
           <Route exact path="/canvas" element={<Canvas />} />
-          <Route exact path="/canvas/edit/:id" element={<Canvas edit />} />
+          <Route
+            exact
+            path="/canvas/edit/:id"
+            element={
+              <ProtectedRoute>
+                <Canvas edit />
+              </ProtectedRoute>
+            }
+          />
           <Route exact path="/about" element={<AboutUs />} />
         </Routes>
       </Router>
