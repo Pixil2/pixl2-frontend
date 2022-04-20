39614232acc81b0d70d0f78fdec5e62b24b2afe7
ajoy267 <andrew.joy12@hotmail.com>
2022-04-20 10:48:44 -0700

    got my test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,21 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home/Home';
 import { UserProvider } from './context/UserContext';
 
 it('Renders PIXL2', async () => {
   render(
     <UserProvider>
       <MemoryRouter>
-        <App />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </MemoryRouter>
     </UserProvider>
   );
-  const header = await screen.findByRole('heading', { name: /pixl 2/i });
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
 
+  const header = await screen.findByRole('heading', { name: /pixl 2/i });
   expect(header).toBeInTheDocument();
 });
