6c80a05097c2787fcf3ed8abd84ea6c76a09b9ac
James Demiraiakian <james.armen@gmail.com>
2022-04-20 16:34:20 -0700

    wrote snapshots for Profile.jsx and Community.jsx

---
+++ src/views/Community/Community.test.jsx
@@ -0,0 +1,41 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { UserProvider } from '../../context/UserContext';
+import Community from './Community';
+
+it('Tests functionality of profile view', async () => {
+  const container = render(
+    <UserProvider>
+      <MemoryRouter initialEntries={['/profile']}>
+        <Routes>
+          <Route
+            exact
+            path="/profile"
+            element={
+              <Community
+                user={{
+                  id: 1,
+                  username: 'whatever',
+                }}
+              />
+            }
+          />
+        </Routes>
+      </MemoryRouter>
+    </UserProvider>
+  );
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+  await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+  screen.debug();
+  const createButton = await screen.findByRole('button', {
+    name: /create image/i,
+  });
+  expect(createButton).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
+});
