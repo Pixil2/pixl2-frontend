97f388f207375751f8398851bcaf402a1fe915f7
James Demiraiakian <james.armen@gmail.com>
2022-04-20 17:27:12 -0700

    tried adding MSW

--- src/views/Profile/Profile.test.jsx
+++ src/views/Profile/Profile.test.jsx
@@ -3,6 +3,8 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import { UserProvider } from '../../context/UserContext';
 import Profile from './Profile';
@@ -18,24 +20,34 @@ const image = {
   isApproved: null,
 };
 
+const user = {
+  id: 1,
+  username: 'whatever',
+};
+
+const server = setupServer(
+  rest.get('/me', (req, res, ctx) => {
+    return res(ctx.json(user));
+  }),
+  rest.get('/images', (req, res, ctx) => {
+    return res(ctx.json(image));
+  })
+);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterAll(() => {
+  server.close();
+});
+
 it('Tests functionality of profile view', async () => {
   const container = render(
     <UserProvider>
       <MemoryRouter initialEntries={['/profile']}>
         <Routes>
-          <Route
-            exact
-            path="/profile"
-            element={
-              <Profile
-                user={{
-                  id: 1,
-                  username: 'whatever',
-                }}
-                image={{ image }}
-              />
-            }
-          />
+          <Route exact path="/profile" element={<Profile />} />
         </Routes>
       </MemoryRouter>
     </UserProvider>
